0be1ccd30be1fde14294557a6e227645977f01c6
Daniel Garrido <dgarrido@plainconcepts.com>
2021-01-24 22:04:25 +0100

    Output ssl_certificates

--- output.tf
+++ output.tf
@@ -18,6 +18,10 @@ output "backend_address_pools" {
   value       = azurerm_application_gateway.appgw.backend_address_pool
   description = "Blocks containing configuration of each backend address pool."
 }
+output "ssl_certificates" {
+  value       = azurerm_application_gateway.appgw.ssl_certificate
+  description = "Blocks containing configuration of each ssl certificate."
+}
 output "http_listeners" {
   value       = azurerm_application_gateway.appgw.http_listener
   description = "Blocks containing configuration of each http listener."
