eb7bdae5280c30514bbe405aa271040e8e0104f8
Daniel Garrido Sánchez <gareda95@outlook.es>
2024-07-27 09:36:30 +0000

    fix: public ip with multizone in test

--- tests/environment/main.tf
+++ tests/environment/main.tf
@@ -42,6 +42,7 @@ resource "azurerm_public_ip" "pip" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   tags                = azurerm_resource_group.rg.tags
+  zones               = [1, 2, 3]
   allocation_method   = "Static"
   sku                 = "Standard"
 }
